c803a5db7768146333ccdc6dac1e25383e9e437f
httpjamesm <github@httpjames.space>
2022-06-10 20:38:15 -0400

    debounce auto save

--- src/components/Slideover.tsx
+++ src/components/Slideover.tsx
@@ -1,6 +1,22 @@
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { Task } from "src/interfaces/task";
-import { decrypt } from "src/utils/aes";
+import { decrypt, encrypt } from "src/utils/aes";
+
+type returnvalue = [string, Dispatch<SetStateAction<string>>];
+
+function useSaveDebounce(delay = 350): returnvalue {
+    const [search, setSearch] = useState<string>("");
+    const [searchQuery, setSearchQuery] = useState<string>("");
+
+    useEffect(() => {
+        const delayFn = setTimeout(() => {
+            setSearch(searchQuery);
+        }, delay);
+        return () => clearTimeout(delayFn);
+    }, [searchQuery, delay]);
+
+    return [search, setSearchQuery];
+}
 
 const Slideover = ({
     task,
@@ -9,9 +25,14 @@ const Slideover = ({
     task: Task;
     cryptoKey: CryptoKey;
 }) => {
-    const [taskName, setTaskName] = useState<string>("");
+    const [nameChanged, setNameChanged] = useState<boolean>(false);
+    const [descChanged, setDescChanged] = useState<boolean>(false);
 
-    const [taskDesc, setTaskDesc] = useState<string>("");
+    const [taskName, setTaskName] = useSaveDebounce(500);
+    const [taskDesc, setTaskDesc] = useSaveDebounce(500);
+
+    const [cachedTaskName, setCachedTaskName] = useState<string>("");
+    const [cachedTaskDesc, setCachedTaskDesc] = useState<string>("");
 
     const decryptData = async () => {
         const decryptedName = await decrypt(
@@ -28,26 +49,92 @@ const Slideover = ({
 
         setTaskName(dec.decode(decryptedName));
         setTaskDesc(dec.decode(decryptedDesc));
+        setCachedTaskName(dec.decode(decryptedName));
+        setCachedTaskDesc(dec.decode(decryptedDesc));
     };
 
     useEffect(() => {
         decryptData();
     }, []);
 
+    useEffect(() => {
+        saveTask();
+    }, [taskName, taskDesc]);
+
+    const saveTask = async () => {
+        const enc = new TextEncoder();
+
+        if (nameChanged) {
+            const encryptedName = await encrypt(
+                enc.encode(taskName),
+                cryptoKey
+            );
+            task.nameCiphertext = encryptedName.data;
+            task.nameIV = encryptedName.iv;
+        }
+
+        if (descChanged) {
+            const encryptedDesc = await encrypt(
+                enc.encode(taskDesc),
+                cryptoKey
+            );
+            task.descriptionCiphertext = encryptedDesc.data;
+            task.descriptionIV = encryptedDesc.iv;
+        }
+
+        const request = await fetch(
+            `${process.env.REACT_APP_API_URL}/task/edit?task=${task.id}`,
+            {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    authorization: window.localStorage.getItem(
+                        "session"
+                    ) as string,
+                },
+                body: JSON.stringify({
+                    name: {
+                        ciphertext: task.nameCiphertext,
+                        iv: task.nameIV,
+                    },
+                    description: {
+                        ciphertext: task.descriptionCiphertext,
+                        iv: task.descriptionIV,
+                    },
+                }),
+            }
+        );
+
+        const response = await request.json();
+
+        if (response.success) {
+            setNameChanged(false);
+            setDescChanged(false);
+        }
+    };
+
     return (
         <>
             <h3>Name</h3>
             <input
                 placeholder="Task Name"
-                value={taskName}
-                onChange={(e) => setTaskName(e.target.value)}
+                value={cachedTaskName}
+                onChange={(e) => {
+                    setNameChanged(true);
+                    setTaskName(e.target.value);
+                    setCachedTaskName(e.target.value);
+                }}
                 style={{ width: "100%" }}
             />
             <h3 style={{ marginTop: "1rem" }}>Description</h3>
             <textarea
                 placeholder="Task Description"
-                value={taskDesc}
-                onChange={(e) => setTaskDesc(e.target.value)}
+                value={cachedTaskDesc}
+                onChange={(e) => {
+                    setDescChanged(true);
+                    setTaskDesc(e.target.value);
+                    setCachedTaskDesc(e.target.value);
+                }}
             />
         </>
     );
